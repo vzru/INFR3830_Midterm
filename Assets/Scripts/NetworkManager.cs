c0b9d2c2a0b0a079f3d1fd861f457b4e295b7b7f
Josh Wessel <joshua.wessel@ontariotechu.net>
2020-03-05 11:01:58 -0500

    End of In-Class Push
    Added Network Manager, A second paddle, and basic UI

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject paddle1;
+    public GameObject paddle2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Paddle1 Position: " + paddle1.transform.position.x);
+        Debug.Log("Paddle2 Position: " + paddle2.transform.position.x);
+
+        ball.GetComponent<BallMovement>();
+        //Debug.Log("Ball Velocity: " + ball.xspeed + ", " + ball.yspeed);
+        
+    }
+}
